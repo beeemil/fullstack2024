a295258d69a0c500b83a487b2fb7b3bd33371bee
beeemil <eemil.becker@helsinki.fi>
2024-09-01 17:17:13 +0300

    fixes / modifications to part 4 based on other parts

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -5,36 +5,34 @@ const jwt = require('jsonwebtoken')
 
 
 blogsRouter.get('/', async (request, response) => {
-    
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-    
-    if (!request.token) {
-      console.log('AAAAA')
-      return response.status(401).json({ error: 'missing token' })
-    }
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: 'token invalid' })
-    }
-    const user = await User.findById(decodedToken.id)
+  if (!request.token) {
+    return response.status(401).json({ error: 'missing token' })
+  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      user: user._id
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    comments:body.comments,
+    user: user._id
+  })
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.status(201).json(savedBlog)    
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)    
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -54,33 +52,44 @@ blogsRouter.delete('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id)
   if (blog.user.toString() === user._id.toString()){
     await Blog.findByIdAndDelete(request.params.id)
+    user.blogs = user.blogs.filter(blogId => blogId.toString() !== request.params.id)
+    await user.save()
     response.status(204).end()
   } else {
     response.status(401).end()
   }
-
 })
 
-blogsRouter.put('/:id', async (request, response) => {
-  if (!request.user) {
-    return response.status(401).end()//.json({ error: 'missing token' })
-  }
-  const user = request.user
+blogsRouter.post('/:id/comments', async (request, response) => {
   const body = request.body
   const blog = {
     title:body.title,
     author:body.author,
     url:body.url,
     likes:body.likes,
+    comments:body.comments,
     user:body.user
   }
-  
-  if (blog.user.toString() === user._id.toString()){
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    response.json(updatedBlog)
-  } else {
-    response.status(401).end()
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  if (!request.user) {
+    return response.status(401).end()
   }
+  const body = request.body
+  const blog = {
+    title:body.title,
+    author:body.author,
+    url:body.url,
+    likes:body.likes,
+    commetns:body.comments,
+    user:body.user
+  }
+  console.log('id',request.params.id)
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- osa4/utils/logger.js
+++ osa4/utils/logger.js
@@ -1,14 +1,14 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  //if (process.env.NODE_ENV !== 'test') {
     console.log('nodeenv', process.env.NODE_ENV)
     console.log(...params)
-  }
+  //}
 }
   
   const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
+  //if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
-  }
+  //}
 }
   module.exports = {
     info, error
